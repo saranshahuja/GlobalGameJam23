75c8946c85185c434a9153ef4dc9d39f51d2139a
Smyles1105 <mylesw6575@gmail.com>
2023-02-02 17:08:55 -0800

    Initial UI

---
+++ Assets/Scripts/QuitButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitButton : MonoBehaviour
+{
+    public void Quit()
+    {
+        Debug.Log("Quitting!");
+        Application.Quit();
+        Debug.Log("Quit!");
+    }
+}
