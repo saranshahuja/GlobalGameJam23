7537f337f4318261f2629087aa9348954945afc2
Smyles1105 <mylesw6575@gmail.com>
2023-02-03 16:13:02 -0800

    working pause overlay and more buttons

---
+++ Assets/ZoomIn.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ZoomIn : MonoBehaviour
+{
+    // public float duration;
+    // public Vector3 targetScale;
+    // public Vector3 originalScale = gameObject.transform.localScale;
+
+    // private Image image;
+    
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     image = GetComponent<Image>();
+    //     originalScale = image.rectTransform.localScale;
+    //     //LeanTween.scale(image.rectTransform, targetScale, duration).setEaseInOutQuad();
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
